dfb824c4f2b639cdb4fc81131f95bfd6671b3dd1
James Willson <jamessw156@gmail.com>
2025-10-21 22:35:18 -0500

    search model logic is now implemented

--- internal/tui/searchmodel.go
+++ internal/tui/searchmodel.go
@@ -1,21 +1,33 @@
 package tui
 
 import (
+	"strings"
+
 	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
+type mode int
+
+const (
+	searchMode mode = iota
+	normalMode
+)
+
 var (
-	searchInputStyle = lipgloss.NewStyle().Border(lipgloss.NormalBorder())
-	searchListStyle  = lipgloss.NewStyle().Border(lipgloss.NormalBorder())
-	cursorStyle      = lipgloss.NewStyle()
+	searchInputStyle          = lipgloss.NewStyle().Border(lipgloss.NormalBorder())
+	unfocusedSearchInputStyle = lipgloss.NewStyle().Border(lipgloss.NormalBorder()).BorderForeground(lipgloss.Color("#5c5c5c"))
+	searchListStyle           = lipgloss.NewStyle().Border(lipgloss.NormalBorder())
+	unfocusedSearchListStyle  = lipgloss.NewStyle().Border(lipgloss.NormalBorder()).BorderForeground(lipgloss.Color("#5c5c5c"))
+	cursorStyle               = lipgloss.NewStyle()
 )
 
 type SearchModel struct {
 	input textinput.Model
 	list  list.Model
+	focus mode
 	app   *ApplicationTUI
 }
 
@@ -23,15 +35,17 @@ func MakeSearchModel(a *ApplicationTUI, items []list.Item, searchText string, li
 	ti := textinput.New()
 	ti.Placeholder = searchText
 	ti.Cursor.Style = cursorStyle
-	// ti.Focus()
+	ti.Focus()
 	frameW, _ := searchInputStyle.GetFrameSize()
 	ti.Width = max(max(listPaneWidth-frameW, 0), lipgloss.Width(searchText))
+
 	list := list.New(items, listDelegate, 0, 0)
 	list.SetShowTitle(false)
 	list.SetShowHelp(false)
 	list.SetShowFilter(false)
 	list.SetShowStatusBar(false)
-	return &SearchModel{input: ti, list: list, app: a}
+	list.DisableQuitKeybindings()
+	return &SearchModel{input: ti, list: list, focus: searchMode, app: a}
 }
 
 func (sm *SearchModel) Init() tea.Cmd {
@@ -39,23 +53,63 @@ func (sm *SearchModel) Init() tea.Cmd {
 }
 
 func (sm *SearchModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	if _, ok := msg.(tea.WindowSizeMsg); ok {
+	prev := sm.input.Value()
+	var ic tea.Cmd
+	sm.input, ic = sm.input.Update(msg)
+	query := sm.input.Value()
+	if prev != query {
+		sm.list.SetFilterText(strings.TrimSpace(query))
+	}
+
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
 		frameW, frameH := searchListStyle.GetFrameSize()
 		listWidth := max(listPaneWidth-frameW, 0)
 		listHeight := max(listPaneHeight-frameH, 0)
 		sm.list.SetSize(listWidth, listHeight)
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "esc":
+			if sm.focus == searchMode {
+				sm.switchToNormal()
+				return sm, nil
+			} else {
+				return sm, tea.Quit
+			}
+		case "i", ":", "/":
+			if sm.focus == normalMode {
+				sm.switchToSearch()
+			}
+		}
 	}
-	var ic tea.Cmd
-	sm.input, ic = sm.input.Update(msg)
+
 	var lc tea.Cmd
 	sm.list, lc = sm.list.Update(msg)
 	return sm, tea.Batch(ic, lc)
 }
 
 func (sm *SearchModel) View() string {
+	var inSty, listSty lipgloss.Style
+	if sm.focus == searchMode {
+		inSty = searchInputStyle
+		listSty = unfocusedSearchListStyle
+	} else {
+		inSty = unfocusedSearchInputStyle
+		listSty = searchListStyle
+	}
 	return lipgloss.JoinVertical(
 		lipgloss.Left,
-		searchInputStyle.Width(listPaneWidth).Render(sm.input.View()),
-		searchListStyle.Width(listPaneWidth).Render(sm.list.View()),
+		inSty.Width(listPaneWidth).Render(sm.input.View()),
+		listSty.Width(listPaneWidth).Render(sm.list.View()),
 	)
+}
+
+func (sm *SearchModel) switchToSearch() {
+	sm.input.Focus()
+	sm.focus = searchMode
+}
+
+func (sm *SearchModel) switchToNormal() {
+	sm.input.Blur()
+	sm.focus = normalMode
 }
